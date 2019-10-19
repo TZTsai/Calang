72d81090d31e781c69401275948f3cd73bf9d486
TZ William <13818704679@163.com>
2019-10-19 17:27:41 +0800

    impl function in tmp.py

--- tmp.py
+++ tmp.py
@@ -1,7 +1,8 @@
 from operator import *
 from queue import LifoQueue
 
-class stack:
+
+class Stack:
     def __init__(self):
         self.s = LifoQueue()
 
@@ -15,35 +16,65 @@ class stack:
     def empty(self):
         return self.s.empty()
 
+class Env:
+    def __init__(self, bindings={}, parent=None):
+        self.bindings = bindings
+        self.parent = parent
 
-binary_ops = {'+':(add, 1), '-':(sub, 1), '*':(mul, 2), '/':(truediv, 2), '//':(floordiv, 2),
-'^':(pow, 3), '%':(mod, 2), '&':(and_, -1), '|':(or_, -2), '=':(eq, 0), '!=':(ne, 0),
-'<':(lt, 0), '>':(gt, 0), '<=':(le, 0), '>=':(ge, 0)}
-parens = {'(':(False, -10), ')':(True, -10)}
-unitary_ops = {'-':(neg, 4), '!':(not_, 4)}
-op_list = list(binary_ops) + list(unitary_ops)
+    def __setitem__(self, name, val):
+        self.bindings[name] = val
+
+    def __getitem__(self, name):
+        try:
+            return self.bindings[name]
+        except KeyError:
+            if self.parent:
+                return self.parent[name]
+            else:
+                raise KeyError('unbound symbol')
+
+class Function:
+    def __init__(self, args, body, env):
+        self.args = args
+        self.body = body
+        self.parent_env = env
+
+    def __call__(self, *args):
+        new_env = Env(dict(zip(self.args, args), self.parent_env))
+        return eval_pure(self.body, new_env)
+
 
 py_eval = eval
 
-global_env = {}
+global_env = Env()
+valStack = Stack()
+opStack = Stack()
+binary_ops = {'+':(add, 1), '-':(sub, 1), '*':(mul, 2), '/':(truediv, 2), '//':(floordiv, 2),
+'^':(pow, 3), '%':(mod, 2), '&':(and_, -1), '|':(or_, -2), '=':(eq, 0), '!=':(ne, 0),
+'<':(lt, 0), '>':(gt, 0), '<=':(le, 0), '>=':(ge, 0)}
+unitary_ops = {'-':(neg, 4), '!':(not_, 4)}
+op_list = list(binary_ops) + list(unitary_ops)
 
 
 def get_token(exp):
     exp = exp.strip()
+    track_parens = 0
     first_char = exp[0]
 
-    if first_char in parens:
-        return 'paren', first_char, exp[1:]
-    elif first_char == ',':
-        return 'comma', first_char, exp[1:]
-    elif exp[:2] in op_list:
+    if exp[:2] in op_list:
         return 'op', exp[:2], exp[2:]
     elif first_char in op_list:
         return 'op', first_char, exp[1:]
+    elif first_char == '<':
+        close_pos = exp.find('>')
+        return 'func', exp[:close_pos+1], exp[close_pos+1:]
     elif first_char.isdigit():
         type = 'number'
     elif first_char.isalpha() or first_char == '_':
         type = 'name'
+    elif first_char == '(':
+        type = 'paren'
+        track_parens += 1
     else:
         raise SyntaxError('unknown symbol!')
 
@@ -51,22 +82,24 @@ def get_token(exp):
         char = exp[i]
         if char.isspace() or \
         (type == 'number' and not char in '1234567890.') or \
-        (type == 'name' and not (char.isalnum() or char == '_')):
+        (type == 'name' and not (char.isalnum() or char in '_?')) or \
+        (type == 'paren' and track_parens == 0):
              return type, exp[:i], exp[i:]
+        if char == '(': track_parens += 1
+        elif char == ')': track_parens -= 1
     return type, exp, ''
 
 
-def check_valid_name(exp):
-    type, _, rest = get_token(exp)
+def get_name(exp):  
+    # strip the spaces in exp, check if it is a valid name and return it
+    type, name, rest = get_token(exp)
     if not(type == 'name' and rest == ''):
         raise SyntaxError('invalid variable name!')
-
-
-valStack = stack()
-symStack = stack()
+    return name
 
 
 def eval_pure(exp, env=global_env):
+    # inner evaluation, without assignment
     def calc(op):
         if op[1] >= 4:  # unitary op
             valStack.push(op[0](valStack.pop()))
@@ -74,41 +107,48 @@ def eval_pure(exp, env=global_env):
             n2 = valStack.pop()
             n1 = valStack.pop()
             valStack.push(op[0](n1, n2))
-    def push_sym(sym):
-        is_paren = lambda sym: sym[1] == -10
-        while not symStack.empty():
-            last_sym = symStack.pop()
-            if is_paren(last_sym) and is_paren(sym):
-                return  # eliminate parens
-            elif not sym[0] or sym[1] > last_sym[1]:
-                symStack.push(last_sym)
+    def push_op(op):
+        while not opStack.empty():
+            last_op = opStack.pop()
+            if not op[0]:
+                if last_op[0]:
+                    opStack.push(last_op)
+                    break
+                else: return
+            if op[1] > last_op[1]:
+                opStack.push(last_op)
                 break
             else:
-                try: calc(last_sym)
+                try: calc(last_op)
                 except AssertionError:
                     raise SyntaxError('invalid syntax')
-        symStack.push(sym)
+        opStack.push(op)
     prev_type = None
-    symStack.push(parens['('])
+    push_op((None, -10))  # mark the beginning of evaluation
     while exp:
         type, token, exp = get_token(exp)
-        if type == 'number': valStack.push(py_eval(token))
+        if type == 'number':
+            valStack.push(py_eval(token))
         elif type == 'name':
-            if token in env: valStack.push(env[token])
-            else: raise ValueError('unbound variable!')
+            valStack.push(env[token])
         elif type == 'op':
             if (prev_type in (None, 'op')) and token in unitary_ops:
-                push_sym(unitary_ops[token])
-            else: push_sym(binary_ops[token])
+                push_op(unitary_ops[token])
+            else: push_op(binary_ops[token])
         elif type == 'paren':
-            if prev_type == 'name':
-                pass
-            else: push_sym(parens[token])
-        elif type == 'comma':
-            
+            if prev_type == 'name':  # applying a function
+                args = [eval_pure(arg) for arg in token[1:-1].split(',')]
+                f = valStack.pop()
+                valStack.push(f(*args))
+            else:
+                valStack.push(eval_pure(token[1:-1], env))
+        elif type == 'func':
+            args = [get_name(seg) for seg in token[1:-1].split(',')]
+            valStack.push(Function(args, exp, env))
+            break
         else: pass  # perhaps add more functionality here
         prev_type = type
-    push_sym(parens[')'])  # force to complete calculation
+    push_op((None, -10))  # force to complete calculation
     result = valStack.pop()
     return result
 
@@ -120,8 +160,7 @@ def eval(exp):
     if assign_mark < 0:
         return eval_pure(exp)
     else:
-        name = exp[:assign_mark].strip()
-        check_valid_name(name)
+        name = get_name(exp[:assign_mark])
         value = eval_pure(exp[assign_mark+2:])
         global_env[name] = value
 
