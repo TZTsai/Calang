b83b6422d804d320dc97b88781e94a7f354f3203
TZ William <13818704679@163.com>
2019-10-21 20:53:51 +0800

    version 2.5

--- calc.py
+++ calc.py
@@ -4,24 +4,24 @@ import math
 
 class Stack:
     def __init__(self):
-        self.s = []
+        self.pbtrack = []
 
     def push(self, obj):
-        self.s.append(obj)
+        self.pbtrack.append(obj)
     
     def pop(self):
         assert(not self.empty())
-        return self.s.pop()
+        return self.pbtrack.pop()
 
     def peek(self):
         assert(not self.empty())
-        return self.s[-1]
+        return self.pbtrack[-1]
 
     def empty(self):
-        return self.s == []
+        return self.pbtrack == []
 
     def clear(self):
-        self.s = []
+        self.pbtrack = []
 
 
 class calcMachine:
@@ -32,7 +32,7 @@ class calcMachine:
     def __calc(self):  # carry out a single operation
         op = self.ops.pop()
         if op[0] is None: return 'stop'
-        elif op[1] >= 4:  # unitary op
+        elif op[1] == 4:  # unitary op
             self.vals.push(op[0](self.vals.pop()))
         else:
             n2 = self.vals.pop()
@@ -49,7 +49,7 @@ class calcMachine:
     def calc(self): # calculate the whole stack and return the result
         while not self.ops.empty() and self.__calc() != 'stop':
             pass
-        return self.vals.pop()
+        return self.vals.pop() if not self.vals.empty() else None
 
     def push_val(self, val):
         self.vals.push(val)
@@ -96,17 +96,19 @@ class Function:
 
 py_eval = eval
 
-global_env = Env()
+global_env = Env({'ans':[]})
 CM = calcMachine()
+ans_records = []
 
 binary_ops = {'+':(add, 1), '-':(sub, 1), '*':(mul, 2), '/':(truediv, 2),
 '//':(floordiv, 2), '^':(pow, 3), '%':(mod, 2), '&':(and_, -1), '|':(or_, -2),
-'=':(eq, 0), '!=':(ne, 0), '<':(lt, 0), '>':(gt, 0), '<=':(le, 0), '>=':(ge, 0)}
+'=':(eq, 0), '!=':(ne, 0), '<':(lt, 0), '>':(gt, 0), '<=':(le, 0), '>=':(ge, 0),
+'@':(lambda l, i: l[i], 5)}
 unitary_ops = {'-':(neg, 4), '!':(not_, 4)}
 
 op_list = list(binary_ops) + list(unitary_ops)
 
-special_words = set(['if', 'else', 'cases'])
+special_words = set(['ans', 'if', 'else', 'cases'])
 
 builtins = {'sin':math.sin, 'cos':math.cos, 'tan':math.tan, 
 'asin':math.asin, 'acos':math.acos, 'atan':math.atan,
@@ -116,7 +118,8 @@ builtins = {'sin':math.sin, 'cos':math.cos, 'tan':math.tan,
 
 def get_token(exp):
     exp = exp.strip()
-    track_parens = 0
+    pbtrack = lambda p, b: p == 0 and b == 0
+    pbtrack.parens, pbtrack.brackets = 0, 0
     first_char = exp[0]
 
     if exp[:2] in op_list:
@@ -132,27 +135,33 @@ def get_token(exp):
         type = 'name'
     elif first_char == '(':
         type = 'paren'
-        track_parens += 1
+        pbtrack.parens = 1
     elif first_char == '[':
         type = 'list'
-        track_parens += 1
+        pbtrack.brackets = 1
     else:
         raise SyntaxError('unknown symbol!')
 
-    for i in range(1, len(exp)):
+    i = 1   
+    while i < len(exp):
         char = exp[i]
-        if type in ('paren', 'list') and track_parens == 0:
-            return type, token, exp[i:]
+        if type in ('paren', 'list'):
+            if pbtrack(pbtrack.parens, pbtrack.brackets): break
         elif char.isspace() or \
         (type == 'number' and char not in '1234567890.') or \
         (type == 'name' and not (char.isalnum() or char in '_?')):
-            token, rest = exp[:i], exp[i:]
-            if token in special_words:
-                return token, token, rest
-            return type, token, rest
-        if char in '([': track_parens += 1
-        elif char in ')]': track_parens -= 1
-    return type, exp, ''
+            break
+        if char == '(': pbtrack.parens += 1
+        elif char == ')': pbtrack.parens -= 1
+        elif char == '[': pbtrack.brackets += 1
+        elif char == ']': pbtrack.brackets -= 1
+        i += 1
+    if pbtrack.parens != 0 or pbtrack.brackets != 0:
+        raise SyntaxError('unpaired parentheses or brackets!')
+    token, rest = exp[:i], exp[i:]
+    if token in special_words:
+        return token, token, rest
+    return type, token, rest
 
 
 def get_name(exp, no_rest=True):  
@@ -213,7 +222,15 @@ def eval_pure(exp, env):
             continue
         if all(t in ('number', 'name', 'paren') for t in (type, prev_type)):
             CM.push_op(binary_ops['*'])
-        if type == 'number':
+        if type == 'ans':
+            if len(global_env['ans']) > 0:
+                if exp and exp[0] == '.':
+                    _, index, exp = get_token(exp[1:])
+                    index = eval_pure(index, env)
+                else: index = -1
+                CM.push_val(global_env['ans'][index])
+            else: raise ValueError('No previous result!')
+        elif type == 'number':
             CM.push_val(eval_number(token))
         elif type == 'name':
             CM.push_val(builtins[token] if token in builtins else env[token])
@@ -252,7 +269,7 @@ def eval(exp):
     # two cases: assignment and evaluation
     assign_mark = exp.find(':=')
     if assign_mark < 0:
-        return eval_pure(exp, global_env)
+        result = eval_pure(exp, global_env)
     else:
         name, rest = get_name(exp[:assign_mark], False)
         if name in special_words or name in builtins:
@@ -267,22 +284,31 @@ def eval(exp):
             value = Function(get_params(para_str), right_exp, global_env)
             # a function is regarded as a special value
         global_env[name] = value
+        result = None
+    global_env['ans'].append(result)
+    return result
 
 
 def repl():
+    count = 0
     while True:
-        exp = input('> ')
         try:
+            exp = input('[{n}]> '.format(n=count))
             val = eval(exp)
             if val is not None: print(val)
-        except KeyboardInterrupt:
-            return
-        except (ValueError, SyntaxError, ArithmeticError, KeyError) as err:
+            count += 1
+        except KeyboardInterrupt: return
+        except (ValueError, SyntaxError, ArithmeticError,
+        KeyError, IndexError) as err:
             CM.reset()
             print(err)
 
 
-tests = """[1,2,3] #[1,2,3]
+### TEST ###
+tests = """[1, 2, 3] #[1,2,3]
+ans #[1,2,3]
+ans@2 #3
+ans.0 #[1,2,3]
 """.splitlines()
 
 def test():
@@ -291,12 +317,13 @@ def test():
         print('>', exp, '' if ans == '' else '(expect: %s)'%ans)
         if result is not None:
             print(result)
-            if result == py_eval(ans):
+            if ans != '' and result == py_eval(ans):
                 print('^ OK!')
             else:
                 print('^ Fail!')
                 return
     print('\nCongratulations, tests all passed!')
+### TEST ###
 
 
 from sys import argv
