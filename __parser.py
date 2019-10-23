3d0bc3406a6bbc44b14fc52fc11934e4445e6bd9
TZ William <13818704679@163.com>
2019-10-23 11:52:08 +0800

    support scientific notation

--- __parser.py
+++ __parser.py
@@ -16,8 +16,8 @@ def get_token(exp):
     elif first_char == '{':
         close_brace = exp[1:].find('}') + 1
         return 'lambda', exp[:close_brace+1], exp[close_brace+1:]
-    elif first_char.isdigit():
-        type = 'number'
+    elif first_char.isdigit() or first_char in '.':
+        type = 'number'; e = 0
     elif first_char.isalpha() or first_char == '_':
         type = 'name'
     elif first_char == '(':
@@ -32,19 +32,30 @@ def get_token(exp):
     i = 1   
     while i < len(exp):
         char = exp[i]
+
         if type in ('paren', 'list'):
             if pbtrack(pbtrack.parens, pbtrack.brackets): break
+
         elif char.isspace() or \
-        (type == 'number' and char not in '1234567890.') or \
+        (type == 'number' and
+            (char not in '1234567890.e' and not (e and char == '-'))) or \
         (type == 'name' and not (char.isalnum() or char in '_?')):
             break
+
+        if type == 'number' and char == 'e':
+            if e > 0: raise SyntaxError
+            e += 1
+
         if char == '(': pbtrack.parens += 1
         elif char == ')': pbtrack.parens -= 1
         elif char == '[': pbtrack.brackets += 1
         elif char == ']': pbtrack.brackets -= 1
+
         i += 1
+
     if pbtrack.parens != 0 or pbtrack.brackets != 0:
         raise SyntaxError('unpaired parentheses or brackets!')
+
     token, rest = exp[:i], exp[i:]
     if token in special_words:
         return token, token, rest
