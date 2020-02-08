0814eecbceac531d36bd3a6d20c2ec2ba3509717
tztsai <13818704679@163.com>
2020-02-08 15:32:19 +0800

    temp update

--- .replace.py
+++ .replace.py
@@ -9,7 +9,7 @@ def replace(*subs):
         splits = []
         k = 0
         for g in range(groups):
-            i, j = m.span(g+1)
+            i, j = [x - m.start() for x in m.span(g+1)]
             splits.append(s[k:i])
             splits.append(subs[g])
             k = j
@@ -17,15 +17,31 @@ def replace(*subs):
     return repl
 
 
+def sub_with(s):
+    return re.sub(r'(\{)[^\{]*:[^\{]*(\})', replace('with ', ':'), s)
+
+def sub_func(s):
+    return re.sub(r'(\{)[^\{]*(\})', replace('function ', ':'), s)
+
 def repfile(filename):
     with open(filename, 'r') as fi, open(filename+'(1)', 'w') as fo:
         s = fi.read()
-        s = re.sub(r'(\{).*:.*(\})', replace('with ', ':'), s)
-        s = re.sub(r'(\{).*(\{)', replace('function ', ':'), s)
+        s = sub_func(s)
+        s = sub_with(s)
         fo.write(s)
 
+def copyfile(filename):
+    with open(filename, 'w') as fo, open(filename+'(1)', 'r') as fi:
+        s = fi.read()
+        fo.write(s)
+
+
+print(sub_func('f := {x, y} x*(1+y)'))
+print(sub_func('compose := {f, g} {x} f(g(x))'))
+print(sub_with('{x:1} x+3'))
 
 for fn in ('tests', *map(lambda s: 'examples/'+s,
                          ('fold', 'lambda_list', 'merge_sort', 'perms', 'tree')),
            *map(lambda s: 'modules/'+s, ('la', 'util'))):
     repfile(fn)
+    # copyfile(fn)
